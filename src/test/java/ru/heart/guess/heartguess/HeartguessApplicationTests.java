96d6ac4ae352368bce99f953b11cd8a19f8c9b8c
HaHhaRr <patrakov.za@mail.ru>
2024-10-31 17:03:42 +0300

    Add files via upload

---
+++ src/test/java/ru/heart/guess/heartguess/HeartguessApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.heart.guess.heartguess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartguessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
